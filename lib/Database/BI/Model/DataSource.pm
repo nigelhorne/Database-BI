13e3061c669972e1192b8408427717ce3bfdd72f
Nigel Horne <njh@bandsman.co.uk>
2026-08-27 21:36:35 -0400

    Release 0.004.0

--- lib/Database/BI/Model/DataSource.pm
+++ lib/Database/BI/Model/DataSource.pm
@@ -12,7 +12,7 @@ use Sub::Protected;
 use Params::Validate::Strict qw(validate_strict);
 use Params::Get		();
 
-our $VERSION = '0.003.2';
+our $VERSION = '0.004.0';
 
 =head1 NAME
 
@@ -20,7 +20,7 @@ Database::BI::Model::DataSource - Table-agnostic adapter around Database::Abstra
 
 =head1 VERSION
 
-Version 0.003.2
+Version 0.004.0
 
 =head1 SYNOPSIS
 
